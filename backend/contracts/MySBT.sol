2ad056e6b7fbe70667de99f408f65896a1377ddd
mumeen <azeezmumeen6@gmail.com>
2022-08-04 13:48:44 +0100

    added whitelist function

--- backend/contracts/MySBT.sol
+++ backend/contracts/MySBT.sol
@@ -6,15 +6,22 @@ import "./ERC721.sol";
 
 contract MySBT is ERC721 {
     uint256 Counter;
+    uint256 whitelistCount;
+    mapping(address => uint256) whitelist;
 
     constructor() ERC721("MySBT", "MSBT") {}
+
+    function whiteList(address to) public {
+        whitelistCount += 1;
+        whitelist[to] = whitelistCount;
+    }
 
     function totalSupply() public view returns (uint256) {
         return Counter;
     }
 
     function safeMint(address to) public {
-        Counter++;
+        Counter += 1;
         _safeMint(to, Counter);
     }
 }
