017feb3a513e7d9c6142f7d75a37023e63af572f
John Montejano <us4john@gmail.com>
2022-08-04 16:22:45 -0700

    [add]hero-desktop]

--- svelte-frontend/src/lib/components/Nav.svelte
+++ svelte-frontend/src/lib/components/Nav.svelte
@@ -1,6 +1,6 @@
 <nav>
   <div class="container flex flex-between ">
-    <a href="/" class='nav-logo'>ERC-SBT</a>
+    <a href="/" class='nav-logo'>ERC-<span>SBT</span></a>
     <ul class="flex">
       <li><a href="/profiles" class="nav-link">Profiles</a></li>
       <li><a href="#" class="btn btn-primary">Connect Wallet</a></li>
--- svelte-frontend/src/routes/index.svelte
+++ svelte-frontend/src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import Landing from '../routes/Landing.svelte'
 </script>
 
-<main class="flex flex-col p-10 w-screen h-screen items-center gap-5 font-serif">
-	<h1 class="text-5xl text-black font-bold">BolzenLabs</h1>
-</main>
+<body>
+	<Landing/>
+</body>
