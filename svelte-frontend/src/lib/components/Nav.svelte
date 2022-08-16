e7236f3d8b76808a11b8928ec0fb6e0c4467f401
mumeen <azeezmumeen6@gmail.com>
2022-08-16 23:28:16 +0100

    add network switching

--- svelte-frontend/src/lib/components/Nav.svelte
+++ svelte-frontend/src/lib/components/Nav.svelte
@@ -1,9 +1,17 @@
+<script>
+	import { connectWallet, walletAddress } from '$lib/stores/wallet';
+</script>
+
 <nav>
-  <div class="container flex flex-between ">
-    <a href="/" class='nav-logo'>ERC-<span>SBT</span></a>
-    <ul class="flex">
-      <li><a href="/profiles" class="nav-link">Profiles</a></li>
-      <li><a href="#" class="btn btn-primary">Connect Wallet</a></li>
-    </ul>
-  </div>
+	<div class="container flex flex-between ">
+		<a href="/" class="nav-logo">ERC-<span>SBT</span></a>
+		<ul class="flex">
+			<li><a href="/profiles" class="nav-link">Profiles</a></li>
+			<li>
+				<button on:click={connectWallet} class="btn btn-primary"
+					>{$walletAddress ? $walletAddress : 'Connect Wallet'}</button
+				>
+			</li>
+		</ul>
+	</div>
 </nav>
